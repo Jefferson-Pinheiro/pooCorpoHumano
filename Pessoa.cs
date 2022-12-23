67e8b65a577206de8cb4102564205e0ae05dff54
Jefferson-Pinheiro <101592447+Jefferson-Pinheiro@users.noreply.github.com>
2022-12-23 09:58:25 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCorpoHumano
+{
+    public class Pessoa
+    {
+        public Pessoa(string cabelo)//Valor obrigatório
+        {
+            Olhos = 2;//Valor default
+            Bracos = 2;
+            Pernas = 2;
+            CorCabelo = cabelo;
+        }
+        public Pessoa()
+        {
+        }
+        public string Nome { get; set; }
+        public int Olhos { get; set; }
+        public string CorCabelo { get; set; }
+        public int Bracos { get; set; }
+        public int Pernas { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCorpoHumano
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();//UTILIZANDO CONSTRUTOR DEFAULT
+            p.Nome = "Tiago";
+            p.Bracos = 2;
+            p.Pernas = 2;
+            p.Olhos = 2;
+            p.CorCabelo = "Preto";
+            Console.WriteLine(p.Nome + " possui " + p.Bracos +
+            " braços," + "\n" + p.Pernas + " pernas, \n " + p.Olhos +
+            " olhos e cabelo " + p.CorCabelo + "\n");
+
+            Pessoa p1 = new Pessoa("Loiro");
+            //UTILIZANDO CONSTRUTOR PERSONALIZADO
+            p1.Nome = "Jefferson";
+
+            Console.WriteLine(p1.Nome + " possui " + p1.Bracos +
+            " braços" + ",\n" + p1.Pernas + " pernas, \n " + p1.Olhos +
+            " olhos e cabelo " + p1.CorCabelo);
+
+
+            Console.ReadKey();
+        }
+    }
+}
